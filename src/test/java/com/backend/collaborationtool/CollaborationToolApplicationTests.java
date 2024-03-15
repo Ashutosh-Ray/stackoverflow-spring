aec398ab5dffec0aabc0e705713e9cd26beebea2
I528661 <ashutosh.ray@sap.com>
2024-03-16 02:05:14 +0530

    First Commit

---
+++ src/test/java/com/backend/collaborationtool/CollaborationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.collaborationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
